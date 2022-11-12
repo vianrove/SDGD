d68e2d15be253eb2ce6a7d5364272b86af4ddc87
vianrove <thuanromero@gmail.com>
2022-11-12 10:23:22 -0500

    Base styles and routes implementes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import {
+    BrowserRouter,
+    Route,
+    Routes
+} from "react-router-dom";
+// ROUTES
+import App from './App';
+import NotFound from './routes/notfound';
+import Login from"./routes/login"
+import Register from "./routes/register"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+                <Route path="login" element={<Login />} />
+                    <Route path="register" element={<Register />} />
+
+
+                {/* pagina no encontrada */}            
+                <Route path="*" element={<NotFound />} />
+        </Routes>
+    </BrowserRouter>
+);
---
+++ src/routes/register.jsx
@@ -0,0 +1,7 @@
+export default function Register() {
+    return (
+      <div>
+        <p>This is the register page</p>
+      </div>
+    )
+  }
